ac74c674c963c58f01a72c8d8c39d0fbdfff0f9c
Arnav Gupta <arnavstudies28@gmail.com>
2023-03-23 00:18:30 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <RandomText />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RandomText from './components/RandText.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    RandomText
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+:root {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: black;
 }
 </style>
+
---
+++ src/components/RandText.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h1 class="title">{{ title }}</h1>
+    <p class="text">{{ text }}</p>
+    <div class="null"></div>
+    <button class="button" @click="fetchData">Generate Random Text</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      title: '',
+      text: ''
+    }
+  },
+
+  methods: {
+    async fetchData() {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      const data = response.data[Math.floor(Math.random() * response.data.length)];
+
+      this.title = data.title;
+      this.text = data.body;
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+.title {
+  color: #00e2da;
+  font-style: bold;
+  font-size: 48px;
+  font-weight: 800;
+  max-width: 1080px;
+  text-align: center;
+  margin: 64px auto;
+}
+
+.text {
+  font-style: italic;
+  font-family: 'Times New Roman', Times, serif;
+  font-weight: 600;
+  font-size: 24px;
+  color:azure;
+  max-width: 1344px;
+  margin: 48px auto;
+}
+
+.button {
+  display: block;
+  padding: 28px;
+  font-size: 20px;
+  margin: auto;
+  text-align: center;
+  border-radius: 64px;
+  font-weight: 550;
+  background-color:darkorchid;
+}
+
+.null {
+  margin: 48px;
+  padding: 16px;
+}
+</style>
